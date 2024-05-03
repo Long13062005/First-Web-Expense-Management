148ba1d55f6704509be66991911d2a1504ef93eb
Long13062005 <vulong13062005@gmail.com>
2024-05-03 18:47:39 +0700

    commit second

--- src/main/java/com/example/expense_management/sercurity/SecurityConfig.java
+++ src/main/java/com/example/expense_management/sercurity/SecurityConfig.java
@@ -21,32 +21,28 @@ public class SecurityConfig {
     /*
      * Bộ lọc bảo mật. Được sử dụng để xác thực, phân quyền, chống tấn công độc hại  cho các request
      * */
-//    @Bean
-//    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
-//        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
-//        requestCache.setMatchingRequestParameterName("mySession");
-//        // csrf(AbstractHttpConfigurer::disable) -> chống tấn công CSRF
-//        http.csrf(AbstractHttpConfigurer::disable)
-//                // xác định quyền cho các request
-//                .authorizeHttpRequests((requests) -> requests
-//                        .shouldFilterAllDispatcherTypes(true)
-//                        // Trước dấu / đầu tiên là localhost:8080
-//                        // /** -> chấp nhận mọi request bắt đầu = login
-//                        .requestMatchers("/login/**").permitAll()
-//                        .requestMatchers("/register/**").permitAll()
-//                        .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
-//                        .requestMatchers("/admin/**").hasAnyRole("ADMIN")
-//                        .anyRequest().authenticated()
-//                )
-//                .formLogin((form) -> form
-//                        .loginPage("/login").loginPage("/") // URL của trang đang nhập
-//                        .loginProcessingUrl("/login")  //xử lý đăng nhập tự động
-//                        .defaultSuccessUrl("/user/") // URL mặc định sau khi đăng nhập thành công
-//                        .permitAll()
-//                ).requestCache((cache) -> cache
-//                        .requestCache(requestCache)
-//                )
-//                .logout(LogoutConfigurer::permitAll);// reference method
-//        return http.build();
-//    }
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        HttpSessionRequestCache requestCache = new HttpSessionRequestCache();
+        requestCache.setMatchingRequestParameterName("mySession");
+        HttpSecurity logout = http.csrf(AbstractHttpConfigurer::disable)
+                .authorizeHttpRequests((requests) -> requests
+                        .shouldFilterAllDispatcherTypes(true)
+                        .requestMatchers("/login/**").permitAll()
+                        .requestMatchers("/register/**").permitAll()
+                        .requestMatchers("/user/**").hasAnyRole("USER", "ADMIN")
+                        .requestMatchers("/admin/**").hasAnyRole("ADMIN")
+                        .anyRequest().authenticated()
+                )
+                .formLogin((form) -> form
+                        .loginPage("/login").loginPage("/") // URL of the login page
+                        .loginProcessingUrl("/login")  // URL for processing automatic login
+                        .defaultSuccessUrl("/user/") // Default URL after successful login
+                        .permitAll()
+                ).requestCache((cache) -> cache
+                        .requestCache(requestCache)
+                )
+                .logout(LogoutConfigurer::permitAll);
+        return http.build();
+    }
 }
